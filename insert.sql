77a39db8f2a2ea4157d1cecf84ffa706afd53f5f
Otmane Boumediane <boumediane.otmane@gmail.com>
2025-12-28 02:27:08 +0100

    refactor and remove lucky stuff

---
+++ insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO patient (name, birth_date)
+VALUES ('Ahmed Benali', '1990-03-12'),
+       ('Sara El Mansouri', '1988-07-25'),
+       ('Youssef Haddad', '2001-01-08'),
+       ('Khadija Rahmani', '1995-11-19'),
+       ('Omar Fassi', '1982-05-03'),
+       ('Nadia Ait Lahcen', '1999-09-14'),
+       ('Hamza Berrada', '1993-06-27'),
+       ('Imane Chraibi', '2004-02-21'),
+       ('Mehdi Zerouali', '1986-12-05'),
+       ('Salma Touhami', '1997-08-30');
--- src/Controller/PatientController.php
+++ src/Controller/PatientController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Patient;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,22 +11,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class PatientController extends AbstractController
 {
     #[Route('/patients')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
-        $people = [
-            [
-                'name' => 'Karim',
-                'birth_date' => '01/01/2000'
-            ],
-            [
-                'name' => 'Alarmi',
-                'birth_date' => '01/01/1990'
-            ],
-        ];
+        $patients = $entityManager->getRepository(Patient::class)->findAll();
 
         return $this->render('patients/index.html.twig', [
-            'people' => $people
+            'patients' => $patients
         ]);
     }
 }
-
