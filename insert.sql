1be3c636b9d2545688482cf08f492ae08923bcd2
Otmane Boumediane <boumediane.otmane@gmail.com>
2026-01-10 14:19:32 +0100

    update insert.sql

--- insert.sql
+++ insert.sql
@@ -1,8 +1,6 @@
 insert into "user" (username, roles, password, name, dtype)
-values ('admin', '[
-    "ROLE_ADMIN"
-]', '$2y$13$An2.8XQj07A0NU1jh99LOeQa/X.JPCFI2gbFSobx9qF3kHecJWWby', 'Admin', 'admin');
+values ('admin', '["ROLE_ADMIN"]', '$2y$13$An2.8XQj07A0NU1jh99LOeQa/X.JPCFI2gbFSobx9qF3kHecJWWby', 'Admin', 'admin');
 
 
 insert into admin (id)
-values (1);
+values (33);
